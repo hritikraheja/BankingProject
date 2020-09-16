5e4c23eb311f2109c1f37a680ea6934a12efb975
91805 <hritik.raheja_cs19@gla.ac.in>
2020-09-16 13:50:11 +0530

    created getters and setters for name and account number

--- src/Definition/Passbook.java
+++ src/Definition/Passbook.java
@@ -4,5 +4,23 @@ public class Passbook {
     private long accountNumber;
     private String customerName;
     private double[] deposits = new double[20];
+    private int numberOfDeposits = 0;
     private double[] withdrawals = new double[20];
+    private int numberOfWithdrawals = 0;
+
+    public long getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(long accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
 }
