54dd282d21362ea81e557608859ba821964a6d73
91805 <hritik.raheja_cs19@gla.ac.in>
2020-09-16 14:38:13 +0530

    Created generateAccountNumber method in Branch.java.

--- src/Definition/Branch.java
+++ src/Definition/Branch.java
@@ -3,10 +3,16 @@ package Definition;
 public class Branch {
     private static String bankName = "Canara Bank";
     private String branchName;
+    Customer[] customers = new Customer[100];
     private int numberOfCustomers = 0;
     private double revenue = 0.0;
+    public final double MINIMUM_BALANCE = 1000.0;
 
     public Branch(String branchName) {
         this.branchName = branchName;
+    }
+
+    private long generateAccountNumber(){
+        return (long) 1469000350 + numberOfCustomers;
     }
 }
